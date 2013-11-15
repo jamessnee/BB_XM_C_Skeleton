49bce8be736a170408e45eadb6bb34b19a9b9c44
James Snee <jas250@cam.ac.uk>
2013-11-15 13:03:16 +0000

    Implemented printing function

--- main.c
+++ main.c
@@ -4,17 +4,28 @@
 #define TX_FIFO_E    (1<<5)
 #define RX_FIFO_E    (1<<0)
 
-int main() {
-	// Check the LSR_tx reg
-	while((UART3_LSR & TX_FIFO_E) == 0);
-	
-	// Write out something
-	UART3_THR = '!';
+void printf(char *c);
+
+int main(void) {
+	char *c = "BOOTING...";
+	printf(c);
+
+	// Keep ourselves alive
+	while (1){}
+
+	return 0;
+}
+
+void printf(char *c){
+	while (*c != '\0')
+	{
+		while((UART3_LSR & TX_FIFO_E) == 0);
+		UART3_THR = *c;
+		c++;		
+	}
 
 	// Check again and write out when done
 	while((UART3_LSR & TX_FIFO_E) == 0);
 	UART3_THR = '\n';
-
-	return 1;
 }
 
